51b48c8a0d0519c2eead64a74fda471259e341ab
soda21 <yusuketask@gmail.com>
2022-08-02 14:56:10 +0900

    8/2トップページで検索ボックスに名前検索可能(名前以外もできるように要機能改善)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,10 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 const Home = (userLoginInfo) => {
+  const [search, setSearch] = useState("");
   // console.log(userLoginInfo);
   const [data, setData] = useState([]);
+
   // // firebaseからデータを取得する
   useEffect(() => {
     const unsub = onSnapshot(
@@ -39,24 +41,42 @@ const Home = (userLoginInfo) => {
       }
     }
   };
-
-  console.log(data);
+  console.log(search);
+  // console.log(data);
   return (
     <div>
       <div>
         <hr />
         <div className="manual">
+          <h3>更新履歴</h3>
           <p>
-            新規登録するとこのリストが更新され作成したUSERでログインできます
+            新規登録でUSERを新規追加、新規作成したユーザーのメールとパスワード情報を使ってでログイン可能
           </p>
-          <p>右上のLOGOUTを押すとLogOutされtopページへ飛びます</p>
+          <p>USER情報の削除、更新ができます。</p>
+          <p>右上のLOGOUTを押すとLogOutされtopページへ</p>
+          <p>
+            8/1右上のKPI_MONITORING作成グラフのたたき台、月別ユーザー数推移など今後使っていきたい
+          </p>
+          <p>8/2検索作成。要改善、今はまだ名前のみ検索</p>
           <hr />
-          <h1> 登録User一覧</h1>
-          <Link to="/adduser" className="topbarIconContainer">
-            <button className="useradd">新規ユーザーの追加</button>
-          </Link>
-        </div>
-        <div className="tablewrapper">
+          {/* 検索------------------------------------------------------- */}
+          <div className="seachbox">
+            <label htmlfor="name">
+              <h1>検索(今現在名前のみ　要改善)</h1>
+            </label>
+            <input
+              className="search"
+              placeholder="検索する名前を入力してください..."
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            {/* UserList------------------------------------------------------- */}
+            <hr />
+            <h1> 登録User一覧</h1>
+            <Link to="/adduser" className="topbarIconContainer">
+              <button className="useradd">新規ユーザーの追加</button>
+            </Link>
+          </div>
+          <div className="tablewrapper"></div>
           <div>
             <table>
               <tr>
@@ -67,33 +87,35 @@ const Home = (userLoginInfo) => {
                 <th>削除</th>
                 <th>更新</th>
               </tr>
-              {data.map((user) => (
-                <tr>
-                  <td>{user.name}</td>
-                  <td>{user.email}</td>
-                  <td> {user.phone}</td>
-                  {/* <td> {user.password}</td> */}
-                  <td>
-                    <div
-                      className="deleteButton"
-                      onClick={() => handleDelete(user.id)}
-                    >
-                      削除
-                    </div>
-                  </td>
-                  <td>
-                    {/* emailを渡すときは上のようにする今回は使わなかった・・・ */}
-                    {/* <Link to={`/user/${user.id}`} state={{ email:`${user.email}`}} style={{ textDecoration: "none" }}> */}
-                    <Link
-                      to={`/user/${user.id}`}
-                      state={{ email: `${user.email}` }}
-                      style={{ textDecoration: "none" }}
-                    >
-                      <div className="viewButton">更新</div>
-                    </Link>
-                  </td>
-                </tr>
-              ))}
+              {data
+                .filter((s) => s.name.includes(search))
+                .map((user) => (
+                  <tr>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td> {user.phone}</td>
+                    {/* <td> {user.password}</td> */}
+                    <td>
+                      <div
+                        className="deleteButton"
+                        onClick={() => handleDelete(user.id)}
+                      >
+                        削除
+                      </div>
+                    </td>
+                    <td>
+                      {/* emailを渡すときは上のようにする今回は使わなかった・・・ */}
+                      {/* <Link to={`/user/${user.id}`} state={{ email:`${user.email}`}} style={{ textDecoration: "none" }}> */}
+                      <Link
+                        to={`/user/${user.id}`}
+                        state={{ email: `${user.email}` }}
+                        style={{ textDecoration: "none" }}
+                      >
+                        <div className="viewButton">更新</div>
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
             </table>
           </div>
         </div>
